44530aa9eaed4b941b2f2892a1fdb0bfd2fb2631
Anders Meidahl <Andersmeidahl@gmail.com>
2018-09-27 21:41:01 +0200

    Update

--- ITWEB-M2-Client/src/app/app.component.ts
+++ ITWEB-M2-Client/src/app/app.component.ts
@@ -1,10 +1,101 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'ITWEB-M2-Client';
+// }
+
+import { Component, Inject, Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { MatTableDataSource, MatSort } from '@angular/material'
+import { Observable } from 'rxjs';
+import { Config } from 'protractor';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']})
+
+@Injectable()
 export class AppComponent {
-  title = 'ITWEB-M2-Client';
+  public url: 'http://localhost:3000/exercises'
+  public apps: Exercise[];
+  // public dataSource: Exercise[];
+  public displayedColumns: String[] = ['Exercise', 'Description', 'Set', 'RepsTime'];
+
+  constructor(private http: HttpClient) {
+      // this.http.get<Applications[]>(this.url)
+      // this.showConfigResponse();
+      
+      this.url = 'http://localhost:3000/exercises';
+      this.http.get<Exercise[]>(this.url).subscribe(data => {
+        console.log(data);
+        var exercises:Exercise[] = new Array()
+
+        for (let index = 0; index < data.length; index++) {
+          exercises[index] = data[index] as Exercise;
+        }
+
+        var tetst = data[1] as Exercise
+        this.apps = data as Exercise[]
+      });
+  }
+
+  // showConfigResponse() {
+  //   this.getConfigResponse()
+  //     // resp is of type `HttpResponse<Config>`
+  //     .subscribe(resp => {
+  //       // access the body directly, which is typed as `Config`.
+  //       this.apps = { ... resp.body };
+  //     });
+  // }
+  // getConfigResponse(): Observable<HttpResponse<Exercise[]>> {
+  //   return this.http.get<Exercise[]>(this.url, { observe: 'response' });
+  // }
+
+  // getConfig() {
+  //   // now returns an Observable of Config
+  //   return this.http.get<Applications[]>(this.url)
+  // }
+
 }
+
+interface Exercise {
+  _id: string;
+  Exercise: string;
+  Description: string;
+  Set: string;
+  RepsTime: string;}
+
+
+  // export class Exercises {
+  // exercise: Exercise[];
+  // constructor(obj: any) {
+  //   this.exercise = obj.category as Exercise[];
+  // }
+// }
+  
+  // const ELEMENT_DATA: PeriodicElement[] = [
+  //   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  //   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  //   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  //   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  //   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  //   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  //   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  //   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  //   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  //   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  // ];
+  
+  /**
+   * @title Basic use of `<table mat-table>`
+   */
+  export class TableBasicExample {
+    displayedColumns: string[] = ['Exercise', 'Description', 'Set', 'RepsTime'];
+    // dataSource = ELEMENT_DATA;
+  }
--- ITWEB-M2-Client/src/app/app.module.ts
+++ ITWEB-M2-Client/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
