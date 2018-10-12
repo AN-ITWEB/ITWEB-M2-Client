1318666852fef6d3ff9c6ee61d0145c87b4d8e61
NicklasNielsen <you@example.com>
2018-10-12 09:18:59 +0200

    uncommented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -100,11 +100,11 @@ export class HomeComponent implements OnInit {
 
   public socialSignIn(socialPlatform : string) {
     let socialPlatformProvider;
-    //if(socialPlatform == "facebook"){
-    //  socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
-    //}else if(socialPlatform == "google"){
+    if(socialPlatform == "facebook"){
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    }else if(socialPlatform == "google"){
       socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-    //}
+    }
     
     this.socialAuthService.signIn(socialPlatformProvider).then(
       (userData) => {
